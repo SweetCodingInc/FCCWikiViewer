0173fe28e07ff8edf8be28556e0da96f93024204
Sweet Coding <sweetcodinginc@gmail.com>
2018-02-07 16:19:38 +0530

    added link to source code on github

--- src/components/sidebar.component.js
+++ src/components/sidebar.component.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { SearchInput } from './search-input.component';
-import {SearchOutput} from './search-output.component';
+import { SearchOutput } from './search-output.component';
 
 export const Sidebar = () => {
     return (
         <div className="full f c">
+            <div>
+                <a href="https://github.com/SweetCodingInc/FCCWikiViewer" target="_blank">Github Repo</a>
+            </div>
             <div className="search-input">
-                <SearchInput/>
+                <SearchInput />
             </div>
             <div className="f full">
-                <SearchOutput/>
+                <SearchOutput />
             </div>
         </div>
     );
